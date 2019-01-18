59b16e33c1df43e5b9a2b3c7bb7512a93c069788
Admin <admin@Admins-Mac.local>
2019-01-18 15:57:53 +0300

    Onclick GRs added, tagss given

--- RockPaperScissors/Controller/MainVC.swift
+++ RockPaperScissors/Controller/MainVC.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UIGestureRecognizerDelegate {
 
+    @IBOutlet weak var opMoves: UIStackView!
+    @IBOutlet weak var playerMoves: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        
+        let imgs = playerMoves.subviews.filter({ $0 is MoveImageView })
+        for img in imgs {
+            let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(sender:)))
+            tap.delegate = self // This is not required
+            img.addGestureRecognizer(tap)
+        }
     }
 
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        print(sender.view!.tag)
+    }
 
 }
 
